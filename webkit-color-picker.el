fa7a17ac265f6a14cea4625b57ddca927e002519
Ozan Sener <ozan@ozansener.com>
2018-03-25 08:58:46 +0200

    Add instructions for MELPA

--- webkit-color-picker.el
+++ webkit-color-picker.el
@@ -33,15 +33,18 @@
 
 ;;; Commentary:
 
-;; #+OPTIONS: toc:nil
+;; #+OPTIONS: toc:nil title:nil timestamp:nil
 ;; * webkit-color-picker                                                :README:
 
 ;; Small experiment with embedded a Webkit widgets in a childframe. Requires Emacs 26 compiled with [[https://www.gnu.org/software/emacs/manual/html_node/emacs/Embedded-WebKit-Widgets.html][embedded Webkit Widget support]].
 
-;; #+BEGIN_EXAMPLE
-;; (require 'webkit-color-picker)
-;; (global-set-key (kbd "C-c C-p") 'webkit-color-picker-show)
-;; #+END_EXAMPLE
+;; webkit-color-picker is available on [[https://melpa.org/][MELPA]]. Example configuration using [[https://github.com/jwiegley/use-package][use-package]]:
+
+;; #+BEGIN_SRC emacs-lisp
+;; (use-package webkit-color-picker
+;;   :ensure t
+;;   :bind (("C-c C-p" . webkit-color-picker-show)))
+;; #+END_SRC
 
 ;; ** Screenshot
 ;; [[./screenshots/webkit-color-picker.gif]]
